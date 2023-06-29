8c532b51bbded1f87ee03bec3c1da0610f66ccfd
Rahul Gharat <rahul@dronamobile.com>
2023-06-29 19:17:11 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/dronahq-logo.png',
 
   // Set the production url of your site here
-  url: 'https://rahul-gharat.github.io',
+  url: 'https://rahul-gharat.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
