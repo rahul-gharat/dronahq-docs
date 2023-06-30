3c4e4bef624ca9d81986c311283c744c052e80a8
shibam17 <shibam.dipu@gmail.com>
2023-06-30 15:44:26 +0530

    database doc
    database doc is completed - images with high res are pending

--- src/components/VersionedLink.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import Link from "@docusaurus/Link";
-
-const VersionedLink = ({ to, ...props }) => (
-  <Link to={`/latest${to}`} {...props} />
-);
-
-export default VersionedLink;
