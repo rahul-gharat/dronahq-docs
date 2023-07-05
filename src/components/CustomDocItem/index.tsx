aa2cc24e7478744f4db07955fb3d64b0c7e0f84e
rahulgharat <rahul@dronamobile.com>
2023-07-05 12:00:27 +0530

    modifications from hasura

---
+++ src/components/CustomDocItem/index.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react';
+import ActualDocItem from '@theme/DocItem';
+import CustomFooter from '@site/src/components/CustomFooter';
+import styles from './styles.module.scss';
+import { Redirect } from '@docusaurus/router';
+
+const CustomDocItem = props => {
+  useEffect(() => {
+    // This function is adds <wbr> tags to code blocks within a table
+    // wherever an _ or . is present. We do this since so many environment
+    // variables are incredibly long and the word breaks are not happening
+    // in a user-friendly way.
+    const tables = document.querySelectorAll('table');
+    tables.forEach(table => {
+      const cells = table.querySelectorAll('td');
+      cells.forEach(cell => {
+        const codeBlocks = cell.querySelectorAll('code');
+        codeBlocks.forEach(codeBlock => {
+          codeBlock.innerHTML = codeBlock.innerHTML.replace(/_/g, '_<wbr>');
+          codeBlock.innerHTML = codeBlock.innerHTML.replace(/\./g, '.<wbr>');
+          console.log(codeBlock.innerHTML);
+        });
+      });
+    });
+  }, []);
+
+  // redirect them to the index if they attempt to directly navigate to a path with
+  // _heading_ in it
+  if (props.location.pathname.includes('_heading_')) {
+    return <Redirect to="/dronahq-docs-hasura/introduction/" />;
+  }
+
+  return (
+    <div
+      className={
+        props.location.pathname === `/dronahq-docs-hasura/introduction/`
+          ? `custom_doc_item_wrapper custom_doc_item_wrapper-x-wide`
+          : `custom_doc_item_wrapper ${styles['custom_doc_item_wrapper']}`
+      }
+    >
+      <ActualDocItem {...props} />
+      <div
+        className={
+          props.location.pathname === `/introduction/` || props.location.pathname.includes('overview')
+            ? `custom_doc_item_footer-x-wide`
+            : styles['custom_doc_item_footer']
+        }
+      >
+        <CustomFooter />
+      </div>
+    </div>
+  );
+};
+
+export default CustomDocItem;
---
+++ src/components/CustomFooter/index.tsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from 'react';
+import ReactDOM from 'react-dom';
+import Link from '@docusaurus/Link';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+import ContributionPointer from './ContributionPointer';
+import GithubIcon from '@site/static/icons/github.svg';
+import TwitterIcon from '@site/static/icons/twitter.svg';
+import DiscordIcon from '@site/static/icons/discord.svg';
+import LinkedInIcon from '@site/static/icons/linkedin.svg';
+import YoutubeIcon from '@site/static/icons/youtube.svg';
+import styles from './styles.module.scss';
+import { v4 as uuidv4 } from 'uuid';
+const CustomFooter = () => {
+  const [element, setElement] = useState(null);
+  useEffect(() => {
+    if (!localStorage.getItem('hasuraDocsUserID')) {
+      const userID = uuidv4();
+      localStorage.setItem('hasuraDocsUserID', userID);
+    }
+  }, []);
+
+  useEffect(() => {
+    const editMetaRow = document.querySelector('.theme-doc-footer-edit-meta-row > .col');
+    setElement(editMetaRow);
+    editMetaRow.style.position = 'relative';
+    const div = document.createElement('div');
+    editMetaRow.appendChild(div);
+    ReactDOM.render(<ContributionPointer element={element} />, div);
+  }, [element]);
+
+  return (
+    <footer className={styles['custom-footer-wrapper']}>
+      <div className={styles['logo-wrapper']}>
+        <img src={useBaseUrl('/dronahq-docs-hasura/img/dronahq-name-logo-dark.svg')} className={styles['dark-theme-logo']} />
+        <img src={useBaseUrl('/dronahq-docs-hasura/img/dronahq-name-logo-light.svg')} className={styles['light-theme-logo']} />
+      </div>
+      <div className={styles['copyright']}>{`© ${new Date().getFullYear()} Deltecs Infotech Pvt Ltd. All rights reserved`}</div>
+      <div className={styles['footerSocialIconsWrapper']}>
+        <div className={styles['socialBrands']}>
+          <Link href={'https://github.com/dronahq/docs-latest'} rel="noopener noreferrer" aria-label={'Github'}>
+            <GithubIcon />
+          </Link>
+        </div>
+        <div className={styles['socialBrands']}>
+          <Link href={'https://twitter.com/dronahq'} rel="noopener noreferrer" aria-label={'Twitter'}>
+            <TwitterIcon />
+          </Link>
+        </div>
+        <div className={styles['socialBrands']}>
+          <Link href={'https://discord.com/invite/dronahq'} rel="noopener noreferrer" aria-label={'Discord'}>
+            <DiscordIcon />
+          </Link>
+        </div>
+        <div className={styles['socialBrands']}>
+          <Link
+            href={'https://www.youtube.com/@DronaHQPlatform'}
+            rel="noopener noreferrer"
+            aria-label={'Youtube'}
+          >
+            <YoutubeIcon />
+          </Link>
+        </div>
+        <div className={styles['socialBrands']}>
+          <Link href={'https://www.linkedin.com/company/deltecs-infotech'} rel="noopener noreferrer" aria-label={'Linkedin'}>
+            <LinkedInIcon />
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default CustomFooter;
