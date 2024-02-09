e3fd2d65868a6c8316a12db9255204cf44a49ce4
Harsh Singh <harsh.s@dronamobile.com>
2024-02-09 17:44:42 +0530

    updated

--- src/components/Changelog/RightChangelogCard.tsx
+++ src/components/Changelog/RightChangelogCard.tsx
@@ -3,13 +3,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from "./style.module.scss";
 import AlertCard from './AlertCard';
+import Tags from './Tags';
 
-const RightChangelogCard = ({ title, heading, descriptions, cards, isDarkTheme = false }) => {
+const RightChangelogCard = ({ title, heading, descriptions, cards,timestamp,tags, isDarkTheme = false }) => {
+  const timestampObj = new Date(timestamp);
+  const formattedTimestamp = timestampObj.toLocaleDateString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
 
   const renderDescriptions = () => {
     if (descriptions && Array.isArray(descriptions)) {
       return descriptions.map((desc, index) => (
-        <div key={index} className={`${styles['changelog-category']} changelog-category`}>
+        <div key={index} className={`${styles['changelog-category']} changelog-category`}  >
           <ul className={styles['changelog-category-list']}>
             {Array.isArray(desc) ? (
               desc.map((description, i) => (
@@ -43,11 +46,15 @@ const RightChangelogCard = ({ title, heading, descriptions, cards, isDarkTheme =
       return null;
     }
   };
-
+  let card_id="id"+timestamp;
   return (
-    <div className={`${styles['right-changelogcard']} right-changelogcard mb-5 pt-4 ${isDarkTheme ? styles['dark-mode'] : ''}`}>
-      <div className={`${styles['changelogcard-title']} changelogcard-title`}>{title}</div>
+    <div className={`${styles['right-changelogcard']} right-changelogcard mb-5 pt-4 ${isDarkTheme ? styles['dark-mode'] : ''}`} id={card_id}>
+      <div className={`${styles['changelogcard-title']} changelogcard-title`}><a href={`#${card_id}`}>{title}</a></div>
       <div className={`${styles['changelogcard-heading']} changelogcard-heading mt-2`}>{heading}</div>
+      <div className={`d-flex ${styles['date-tag-container']}`}>
+        <div className={`${styles['changelogcard-date']} changelogcard-date hq-modal-title`}>{formattedTimestamp}</div>
+        <Tags tags={tags} />
+      </div>
       <div className={`${styles['card-descriptions']} card-descriptions mt-2`}>{renderDescriptions()}</div>
       <div className={`${styles['cards-alert']} cards-alert mt-2`}>{renderCards()}</div>
     </div>
