2aa5c6737be39ce6f402c9499701efa2c83d3a01
Harsh Singh <harsh.s@dronamobile.com>
2024-02-12 10:25:52 +0530

    updated

--- src/components/Changelog/Tags.tsx
+++ src/components/Changelog/Tags.tsx
@@ -3,12 +3,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from "./style.module.scss";
 
-const Tags = ({ tags,isDarkTheme=false }) => {
+const Tags = ({ tags, isDarkTheme = false }) => {
+  const capitalizeFirstLetter = (word) => {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  };
+
   return (
-    <div className={`${styles['tags-container']} tags-container`}  >
+    <div className={`${styles['tags-container']} tags-container`}>
       {tags.map((tag, index) => (
         <div key={index} className={`${styles['changelogcard-tag']} hq-tags border small green d-flex align-center pointer justify-center mt-2`}>
-          {tag}
+          {capitalizeFirstLetter(tag)}
         </div>
       ))}
     </div>
