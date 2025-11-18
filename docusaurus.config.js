76d2cade89a9cc3e608a3670184a2e08bc72924d
Rahul Gharat <rahul@dronamobile.com>
2025-11-18 13:04:36 +0530

    Agents tab added

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -222,11 +222,11 @@ const config = {
             label: 'Apps',
             position: 'left',
           },
-          // {
-          //   to: '/agents/getting-started/introduction/',
-          //   label: 'Agents',
-          //   position: 'left',
-          // },
+          {
+            to: '/agents/getting-started/introduction/',
+            label: 'Agents',
+            position: 'left',
+          },
           {
             to: 'https://discord.gg/QfN8yq4mqM',
             label: 'Discord',
